0d15e9e015e97da5c7c3e5deb2ca6562ad9eba5f
wzeddie <89380062@qq.com>
2024-08-07 22:59:01 +0800

    添加自定义mdx

---
+++ src/components/ui/chart.tsx
@@ -0,0 +1,20 @@
+function Chart({ data = [], color }) {
+    return (
+      <div className="snowfall">
+        {data.map((d, i) => (
+          <div
+            key={i}
+            className="snowfall-bar"
+            style={{
+              height: d * 20,
+              backgroundColor: color,
+            }}
+          >
+            <span>{i + 1}月</span>
+          </div>
+        ))}
+      </div>
+    )
+  }
+  
+  export default Chart;
